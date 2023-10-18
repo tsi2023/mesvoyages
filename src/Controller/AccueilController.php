08cf24b4fdc465e73afc67d666c9fa4c8ab839e5
Tsipora Adler <tsipora.pro@gmail.com>
2023-10-18 14:54:45 +0200

    phase 2 : premier test "hello word!"

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AccueilController
+ *
+ * @author tsili
+ */
+class AccueilController {
+    //put your code here
+    /**
+     * @Route("/", name="accueil")
+     * @return Response
+     */
+    public function index(): Response{ 
+        return new Response('Hello world !');
+        
+    }
+}

3037751fe4275ac871275a5f49bb50d2ec0b8b9f
Tsipora Adler <tsipora.pro@gmail.com>
2023-11-10 13:20:51 +0100

    Phase 9 : ORM (tri des enregistrements)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -18,29 +18,44 @@ use Symfony\Component\Routing\Annotation\Route;
  *
  * @author tsili
  */
-class VoyagesController extends AbstractController{
- //put your code here
+class VoyagesController extends AbstractController {
+    //put your code here
+
     /**
      * @Route("/voyages", name="voyages")
      * @return Response
      */
-    public function index(): Response{ 
-        $visites = $this->repository->findAll();
+    public function index(): Response {
+        $visites = $this->repository->findAllOrderBy('datecration', 'DESC');
         return $this->render("pages/voyages.html.twig", [
-            'visites' => $visites
+                    'visites' => $visites
         ]);
     }
-    /**
- * 
- * @var VisiteRepository
- */
-private $repository;
 
-/**
- * 
- * @param VisiteRepository $repository
- */
-public function __construct(VisiteRepository $repository) {
+    /**
+     * 
+     * @var VisiteRepository
+     */
+    private $repository;
+
+    /**
+     * 
+     * @param VisiteRepository $repository
+     */
+    public function __construct(VisiteRepository $repository) {
         $this->repository = $repository;
-}
+    }
+    
+    /**
+     * @Route("/voyages/tri/{champ}/{ordre}", name="voyages.sort")
+     * @param type $champ
+     * @param type $ordre 
+     * @param Response 
+     */
+    public function sort($champ, $ordre): Response{
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig",[
+            'visites' => $visites          
+        ]);
+    }
 }
